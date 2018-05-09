c346f72f4576b80ebf654d7bb4e06da613dc618f
Guest User <Guest@epicodus-7.local>
2018-05-09 15:12:09 -0700

    add WeatherReducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const API_KEY = '5aaa25520e670fecea4305698e5f1296';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
@@ -5,8 +7,10 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
 
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   };
 }
