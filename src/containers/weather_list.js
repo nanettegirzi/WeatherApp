064da9ac85147678cf3ae906f3fc30a00aea3a02
Guest User <Guest@epicodus-4.local>
2018-05-10 10:38:45 -0700

    add chart component

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import Chart from '../components/chart';
+
 
 class WeatherList extends Component {
   renderWeather(cityData){
     const name = cityData.city.name
     const temps = cityData.list.map(weather => weather.main.temp);
-
+    const pressure = cityData.list.map(weather => weather.main.pressure);
+    const humidity = cityData.list.map(weather => weather.main.humidity);
 
     return (
       <tr key={name}>
         <td>{name}</td>
-        <td>
-          <Sparklines height={120} width={180} data={temps}>
-            <SparklinesLine color="blue" />
-          </Sparklines>
-        </td>
+        <td><Chart data={temps} color="blue" units="K" />  </td>
+        <td><Chart data={pressure} color="orange" units="hPa" />  </td>
+        <td><Chart data={humidity} color="black" units="%" />  </td>
       </tr>
     )
   }
@@ -26,9 +26,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temerature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temerature (K)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
